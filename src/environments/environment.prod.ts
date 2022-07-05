92426607611ca34b036f2957ed3df3d88fcad283
Andrew <andrew_shevchuk21@protonmail.com>
2022-07-05 15:08:56 +0300

    first commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+import {Environment} from './interface';
+
+export const environment: Environment = {
+  production: true,
+  apiKey: 'AIzaSyBnjLO0NzcxEBPmxRw6j-A0zsvyKfC_aq8',
+  fbDbUrl: 'https://angular-blog-bcd6d-default-rtdb.europe-west1.firebasedatabase.app'
+};
